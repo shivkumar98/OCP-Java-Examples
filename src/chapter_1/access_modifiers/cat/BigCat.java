811a6ebc6a62921f162f29022062f863a83d0798
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-09-24 11:55:14 +0100

    First commit

---
+++ src/chapter_1/access_modifiers/cat/BigCat.java
@@ -0,0 +1,12 @@
+package chapter_1.access_modifiers.cat;
+
+import javax.swing.plaf.nimbus.AbstractRegionPainter;
+
+public class BigCat {
+	
+	public String breed;
+	protected String owner;
+	int age;
+	private int id;
+
+}
---
+++ src/chapter_1/access_modifiers/cat/species/Lynx.java
@@ -0,0 +1,22 @@
+package chapter_1.access_modifiers.cat.species;
+
+import chapter_1.access_modifiers.cat.BigCat;
+
+public class Lynx extends BigCat {
+	
+	public static void main(String[] args) {
+		BigCat cat = new BigCat();
+		// You can access public field in any class
+		cat.breed = "Lynx";
+		// You can not access protected variables
+		// this codes not compile: cat.owner = "shiv";
+		// a subclass instance can access the default access field
+		Lynx lynx = new Lynx();
+		lynx.owner = "shiv";
+		// You would not be able to access fields if use BigCat as reference
+		BigCat sameLynx= lynx;
+		/* this does not compile:
+		sameLynx.owner = "sammy" */
+	}
+
+}
