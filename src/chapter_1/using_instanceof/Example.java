0cb6b8c9e73bc34e1f802eae0c04ec82af4b0523
shivkumar98 <shiv_kumar.98@outlook.com>
2022-09-26 10:10:46 +0100

    Finished writing examples on instanceof

--- src/chapter_1/using_instanceof/Example.java
+++ src/chapter_1/using_instanceof/Example.java
@@ -4,7 +4,8 @@ package chapter_1.using_instanceof;
  * 'A instanceof B'
  *  returns true if ref of A points to is:
  *  1) an instance of class B
- *  2) a class which implements the B interface (indirect or direct) 
+ *  2) a class which implements the B interface (indirect or direct)
+ *  A common use of instanceof is to determine if an instance is a subclass of a particular object before applying an explicit cast  
  */
 
 
@@ -29,8 +30,20 @@ public class Example {
 		/* however, null is not considered an instance of Object*/
 		Lion lion2 = null;
 		System.out.println(lion2 instanceof Lion); // false
-		System.out.println(lion2 instanceof Object);
+		System.out.println(lion2 instanceof Object); // false
 		
+		Lion lion3 = new Lion();
+		
+		/* the compiler can check whether a object is not related to a class:
+		* E.g.: System.out.println(lion3 instanceof Tiger);  does not compile
+		* However this compiler check only applies for classes and not interfaces
+		*/
+		
+		ScaryAnimal tiger3 = new Tiger();
+		System.out.println(tiger3 instanceof Mother); // false
+		/* even though tiger is not an implementation of Mother,
+		 * the compiler does not do a check
+		 */
 	}
 
 }
@@ -38,3 +51,5 @@ public class Example {
 class ScaryAnimal {}
 class Tiger extends ScaryAnimal {}
 class Lion extends ScaryAnimal {}
+
+interface Mother {}
