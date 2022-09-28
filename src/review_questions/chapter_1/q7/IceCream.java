e322937d29de6c7a343261baf21afc3c5405db52
shivkumar98 <shiv_kumar.98@outlook.com>
2022-09-28 15:30:49 +0100

    Finished Q6-7 of Chapter 1 review

---
+++ src/review_questions/chapter_1/q7/IceCream.java
@@ -0,0 +1,31 @@
+package review_questions.chapter_1.q7;
+/* QUESTION:  What is the result of the following code?
+   OPTIONS:
+	A. vanilla
+	B. chocolate
+	C. strawberry
+	D. missing flavor
+	E. The code does not compile.
+	F. An exception is thrown..
+
+	CORRECT ANSWER: E - the case must be an enumeration
+*/
+public class IceCream {
+	enum Flavours {
+		VANILLA, CHOCOLATE, STRAWBERRY
+	}
+	public static void main(String[] args) {
+		Flavours f = Flavours.STRAWBERRY;
+		switch(f) {
+			case 0: System.out.println("vanilla"); // does not compile
+			case 1: System.out.println("chocolate"); // does not compile
+			case 2: System.out.println("strawberry"); // does not compile
+					break;
+			default: System.out.println("missing flavor");
+		}
+		
+		switch (f) {
+			case CHOCOLATE: System.out.println("chocolate"); // this works
+		}
+	}
+}
