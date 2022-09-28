17e323df2f9ca3b16fd6f1a5a3b8ce59a5459d07
shivkumar98 <shiv_kumar.98@outlook.com>
2022-09-28 14:45:35 +0100

    Added Q1 of Chapter 1 Review

---
+++ src/review_questions/chapter_1/q1/Employee.java
@@ -0,0 +1,36 @@
+package review_questions.chapter_1.q1;
+
+/* QUESTION: What is the result of the following code:
+ * ANSWERS:
+ *  A - Success
+ *  B - Failure
+ *  C - The hashCode() method fails to compile.
+ *	D - The equals() method fails to compile.
+ *	E - Another line of code fails to compile.
+ * 	F - A runtime exception is thrown.
+ * 
+ * CORRECT ANSWER: A 
+ */
+public class Employee {
+	public int employeeId;
+	public String firstName, lastName;
+	public int yearStarted;
+	
+	@Override
+	public int hashCode() {
+		return employeeId;
+	}
+	
+	public boolean equals(Employee e) {
+		return this.employeeId == e.employeeId;
+	}
+	public static void main(String[] args) {
+		Employee one = new Employee();
+		one.employeeId = 101;
+		Employee two = new Employee();
+		two.employeeId = 101;
+		if (one.equals(one))
+			System.out.println("Success");
+		else System.out.println("Failure");
+	}
+}
