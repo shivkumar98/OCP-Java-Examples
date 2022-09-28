faee2fb5f549f4ec063d3789052989166a2937b2
shivkumar98 <shiv_kumar.98@outlook.com>
2022-09-28 15:45:47 +0100

    q8 complete

---
+++ src/review_questions/chapter_1/q8/Outer.java
@@ -0,0 +1,25 @@
+package review_questions.chapter_1.q8;
+/* QUESTION:  What is the result of the following code?
+OPTIONS:
+	A. The output is 5.
+	B. The output is 10.
+	C. Line 16 generates a compiler error.
+	D. Line 20 generates a compiler error.
+	E. Line 21 generates a compiler error.
+	F. An exception is thrown
+
+	CORRECT ANSWER: C - inner classes cannot have static members, only static nested classes can
+*/
+public class Outer {
+	private int x = 5;
+	protected class Inner {
+		public static int x =10; // does not compile
+		public void go() { System.out.println(x); }
+	}
+	public static void main(String[] args) {
+		Outer out = new Outer();
+		Outer.Inner in = out.new Inner();
+		in.go();
+	}
+
+}
