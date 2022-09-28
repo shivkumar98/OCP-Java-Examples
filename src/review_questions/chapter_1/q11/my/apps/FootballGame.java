f10cc189a218f0ac3548f71bdf0f4a79aa791744
shivkumar98 <shiv_kumar.98@outlook.com>
2022-09-28 17:38:52 +0100

    Q11 of chapter 1 review complete

---
+++ src/review_questions/chapter_1/q11/my/apps/FootballGame.java
@@ -0,0 +1,6 @@
+package review_questions.chapter_1.q11.my.apps;
+// 	INSERT IMPORT HERE
+import static review_questions.chapter_1.q11.my.sports.Football.*;
+public class FootballGame {
+	public int getTeamSize() { return TEAM_SIZE;}
+}
---
+++ src/review_questions/chapter_1/q11/my/sports/Football.java
@@ -0,0 +1,5 @@
+package review_questions.chapter_1.q11.my.sports;
+
+public class Football {
+	public static final int TEAM_SIZE = 11;
+}
