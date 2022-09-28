8bbe8923669f5472afc30a9da8e1e6fd683ad12a
shivkumar98 <shiv_kumar.98@outlook.com>
2022-09-28 15:01:54 +0100

    Finished Q2-4 of chapter 1

---
+++ src/review_questions/chapter_1/q4/BaseballTeam.java
@@ -0,0 +1,37 @@
+package review_questions.chapter_1.q4;
+/* QUESTION: What is true about the following code? You may assume city and mascot are never nulL.*/
+/* OPTIONS:
+A. The class does not compile.
+B. The class compiles but has an improper equals() method.
+C. The class compiles but has an improper hashCode() method.
+D. The class compiles and has proper equals() and hashCode() methods.
+
+CORRECT ANSWER: C - the hashCode method violates the rule that two instances which are equal have the same hashCode
+ */
+public class BaseballTeam {
+	private String city, mascot;
+	private int numberOfPlayers;
+	public boolean equals(Object obj) {
+		if (!(obj instanceof BaseballTeam))
+			return false;
+		BaseballTeam other = (BaseballTeam) obj;
+		return (city.equals(other.city) && mascot.equals(other.mascot));
+	}
+	public int hashCode() {
+		return numberOfPlayers;
+	}
+	
+	public static void main(String[] args) {
+		BaseballTeam one = new BaseballTeam();
+		one.numberOfPlayers = 23;
+		one.city = "Birmingham";
+		one.mascot = "Bull";
+		BaseballTeam two = new BaseballTeam();
+		two.numberOfPlayers = 4689;
+		two.city = "Birmingham";
+		two.mascot = "Bull";
+		System.out.println(one.equals(two)); // true
+		System.out.println(one.hashCode() + " "+ two.hashCode()); // 23 4689
+		// hashcods should not differ! :(
+	}
+}
