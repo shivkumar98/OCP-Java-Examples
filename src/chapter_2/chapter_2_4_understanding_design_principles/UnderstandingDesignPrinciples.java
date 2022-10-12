32fcfc5e59dcc984e78be8de8500e682ce833fdc
shivkumar98 <shiv_kumar.98@outlook.com>
2022-10-12 11:10:46 +0100

    Finished example 2 of understanding polymorphism

--- src/chapter_2/chapter_2_4_understanding_design_principles/UnderstandingDesignPrinciples.java
+++ src/chapter_2/chapter_2_4_understanding_design_principles/UnderstandingDesignPrinciples.java
@@ -1,5 +1,13 @@
 package chapter_2.chapter_2_4_understanding_design_principles;
 
+/* Q: What is a deisgn principle?
+ * A: this is an established principle used to facilitate software design principle
+ * Following deisgn principles leads to:
+ * - More logical code
+ * - Code is easier to understand as people can research the deisgn principle
+ * - Allows code to be easier to modify with respect to requirements
+ */
+
 public class UnderstandingDesignPrinciples {
 
 }
