a71ac175fda49198a97fcb0fd6d9603995f9135d
shivkumar98 <shiv_kumar.98@outlook.com>
2022-10-12 18:03:12 +0100

    Finished 2.4 - Understanding Design Principle

---
+++ src/chapter_2/chapter_2_4_understanding_design_principles/s5_composing_objects/ComposingObjects.java
@@ -0,0 +1,6 @@
+package chapter_2.chapter_2_4_understanding_design_principles.s5_composing_objects;
+
+public class ComposingObjects {}
+
+/* Q: What is object-composition?
+ * A: This is the property of creating a class which references another class in order to use its functionality*/
