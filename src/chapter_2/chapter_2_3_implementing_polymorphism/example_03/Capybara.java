7aa255d89cee0c3c6fc1643aaf0f5af671c5fca7
shivkumar98 <shiv_kumar.98@outlook.com>
2022-10-12 10:23:50 +0100

    Finished section 2.3: implementing pol;ymorphism

--- src/chapter_2/chapter_2_3_implementing_polymorphism/example_03/Capybara.java
+++ src/chapter_2/chapter_2_3_implementing_polymorphism/example_03/Capybara.java
@@ -1,7 +1,19 @@
 package chapter_2.chapter_2_3_implementing_polymorphism.example_03;
 
+/* Even if two classes share a hierarchy, casting still may be unlikely to occur without issues
+ * For example, you cannot cast a super class to its subclass without ClassCastException
+ */
+
 class Rodent { }
 
-public class Capybara {
-
+public class Capybara extends Rodent {
+	public static void main(String[] args) {
+		Rodent rodent = new Rodent();
+		// Capybara capybara = (Capybara)rodent; // causes class cast exception
+		
+		// we should use the instanceof operator before casting:
+		if (rodent instanceof Capybara) {
+			Capybara capybara = (Capybara) rodent;
+		}
+	}
 }
