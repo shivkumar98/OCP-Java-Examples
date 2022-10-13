8de360cc550315d17b55576191fa5b05c1bcd613
shivkumar98 <shiv_kumar.98@outlook.com>
2022-10-13 11:48:28 +0100

    Removed classes filled with documentation and added documentation to readme

--- src/chapter_2/chapter_2_4_understanding_design_principles/HasARelationship.java
+++
@@ -1,6 +0,0 @@
-package chapter_2.chapter_2_4_understanding_design_principles;
-public class HasARelationship {}
-/* The has-a test is another object-oriented design. It is also known as composition test
- * The has-a test checks if an object contains a particular property or value.
- * This is different from inheritance, we qare now analysing the COMPOSITION of a class
- */
--- src/chapter_2/chapter_2_5_working_with_design_patterns/README.md
+++ src/chapter_2/chapter_2_5_working_with_design_patterns/README.md
@@ -2,4 +2,4 @@
 
 **ANSWER:** A design pattern is an established paradigm used to solve common problems
   
-- Common design principle is to use level of indirection, where we seperate a task such as creating an object into multiple levels
+Common design principle is to use level of indirection, where we seperate a task such as creating an object into multiple levels
