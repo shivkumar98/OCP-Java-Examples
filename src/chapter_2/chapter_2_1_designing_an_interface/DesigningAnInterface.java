81c8507d93d794ccfed5eee80266dd8d8ba83b1d
shivkumar98 <shiv_kumar.98@outlook.com>
2022-10-13 12:46:13 +0100

    Finished documentation on chapter 2.1 0 designing an interface

--- src/chapter_2/chapter_2_1_designing_an_interface/DesigningAnInterface.java
+++
@@ -1,9 +0,0 @@
-package chapter_2.chapter_2_1_designing_an_interface;
-/* Interface are a means of supporting multiple inheritance in ava
- * An interface is not required to contain methods/variables, some are designed simply to categorise a class which it implements
- *  -> E.g. java.io.Serializable is a marker interface
- *  Interfaces can not be instantiated or marked final
- */
-public class DesigningAnInterface {
-
-}
