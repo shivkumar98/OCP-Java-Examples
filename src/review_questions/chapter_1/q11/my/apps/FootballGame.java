248dee6d72717b5918f844c34a1ff408db53c5e0
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2023-09-02 15:19:46 +0100

    Created rev questions for chap 1

--- src/review_questions/chapter_1/q11/my/apps/FootballGame.java
+++
@@ -1,5 +0,0 @@
-package review_questions.chapter_1.q11.my.apps;
-import static review_questions.chapter_1.q11.my.sports.Football.*;
-public class FootballGame {
-	public int getTeamSize() { return TEAM_SIZE;}
-}
--- src/review_questions/chapter_1/q11/my/sports/Football.java
+++
@@ -1,5 +0,0 @@
-package review_questions.chapter_1.q11.my.sports;
-
-public class Football {
-	public static final int TEAM_SIZE = 11;
-}
