9a29cf51e9ac1237e4c69d42f0507287b50d3870
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2023-10-30 10:38:41 +0000

    🟥 7.3.1 Protecting Data with Atomic Classes V2

--- src/chapter_7_v2/c_7_3_synchronizingDataAccess/javaCode/SheepManager.java
+++ src/chapter_7_v2/c_7_3_synchronizingDataAccess/javaCode/SheepManager.java
@@ -1,22 +1,25 @@
-package chapter_7.c_7_3_synchronizingDataAccess.javaCode;
+package chapter_7_v2.c_7_3_synchronizingDataAccess.javaCode;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SheepManager {
 	private int sheepCount = 0;
-	void incrementAndReport() {
-		System.out.print((++sheepCount)+" ");
+	 void incrementAndReport() {
+		System.out.println((++sheepCount) + " ");
 	}
 	public static void main(String[] args) {
 		ExecutorService service = null;
 		try {
-			SheepManager manager = new SheepManager();
 			service = Executors.newFixedThreadPool(20);
+			SheepManager manager = new SheepManager();
 			for(int i=0;i<10;i++)
-			service.submit(() -> manager.incrementAndReport());
+				service.submit(() -> manager.incrementAndReport());
 		} finally {
 			if(service!=null) service.shutdown();
 		}
 	}
+
 }
+
+
