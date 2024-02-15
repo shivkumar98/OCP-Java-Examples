9e5a4dc175cd8f67a9e636123f123235e3830813
shiv.kumar <shiv_kumar.98@outlook.com>
2024-02-15 12:23:11 +0000

    wip ⭐ Using createDirectory() and createDirectories() ⭐

---
+++ src/chapter_9/c_9_2_interacting_with_paths_files/javacode/MakingDirectories.java
@@ -0,0 +1,36 @@
+package chapter_9.c_9_2_interacting_with_paths_files.javacode;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MakingDirectories {
+	// legacy java.io API used mkdir(), mkdirs()
+	// File.createDirectory(Path) is used to make dirs
+	// will throw IOException if directory cannot be created 
+	// or already exists!
+	
+	public static void main(String[] args) {
+		Path relDirectoryAlreadyExists = Paths
+		  .get("src/chapter_9/" + "c_9_2_interacting_with_paths_files"
+		  		+ "/javacode/");
+		
+		try {
+			System.out.println(Files.createDirectory(relDirectoryAlreadyExists));
+		} catch (IOException e) {
+			System.out.println("exception caught!!!"); // this prints!!!
+		}
+		
+		Path relDirWhichDoesNotExist = Paths
+				.get("src/chapter_9/" + "c_9_2_interacting_with_paths_files"
+				  		+ "/javacode/temp_folder");
+		try {
+			System.out.println(Files.createDirectory(relDirWhichDoesNotExist));
+		} catch (IOException e) {
+			// no exceptions to catch
+		}
+		
+		
+	}
+}
